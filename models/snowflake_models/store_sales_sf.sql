05406c989f0307c8d56ad940cc67f5c2c8c6f19c
Vaidy Kalpathy <vaidykalp@gmail.com>
2024-02-27 09:42:54 +0000

    Included YAML file for source definitions

--- models/snowflake_models/store_sales_sf.sql
+++ models/snowflake_models/store_sales_sf.sql
@@ -1,4 +1,9 @@
 {{ config(materialized='view') }}
 
+/*
 with store_sales_tbl_sf as (select ss_store_sk, ss_quantity, ss_sales_price, ss_net_profit from DBT_DB.PROD.STORE_SALES_SF)
+select * from store_sales_tbl_sf
+*/
+
+with store_sales_tbl_sf as (select ss_store_sk, ss_quantity, ss_sales_price, ss_net_profit from {{ source('sf_prod', 'STORE_SALES_SF') }})
 select * from store_sales_tbl_sf
--- models/snowflake_models/store_sf.sql
+++ models/snowflake_models/store_sf.sql
@@ -1,4 +1,4 @@
 {{ config(materialized='view') }}
 
-with store_tbl_sf as (select s_store_sk, s_store_id, s_store_name, s_country, s_county from DBT_DB.PROD.STORE_SF)
+with store_tbl_sf as (select s_store_sk, s_store_id, s_store_name, s_country, s_county from {{ source('sf_prod', 'STORE_SF') }})
 select * from store_tbl_sf
